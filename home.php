3b9985a4e642e75aa905d49f5a5b86f1e03501b0
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-25 21:15:23 +0300

    Add helpful containers to home

--- home.php
+++ home.php
@@ -20,35 +20,63 @@
 				<i class="smaller">It works on every platform, browser or device, focusing equally on reliability and speed.</i>
 			</p>
 
-			<h2>Real-time analytics</h2>
-			<p>Push data to clients that gets represented as real-time counters, charts or logs.</p>
+			<div id="examples">
+				<div class="example-column left">
+					<div class="example-entry">
+						<h2>Real-time analytics</h2>
+						<p>Push data to clients that gets represented as real-time counters, charts or logs.</p>
+					</div>
 
-			<h2>Binary streaming</h2>
-			<p>Starting in 1.0, it's possible to send any blob back and forth: image, audio, video.</p>
+					<div class="example-entry">
+						<h2>Binary streaming</h2>
+						<p>Starting in 1.0, it's possible to send any blob back and forth: image, audio, video.</p>
+					</div>
+				</div>
 
-			<h2>USED BY EVERYONE</h2>
-			<p>From Microsoft Office, Yammer, Zendesk, Trello... to hackathon winners and little startups.</p>
-			<p>One of the most powerful JavaScript frameworks on GitHub, and most depended-upon NPM module.</p>
+				<div class="example-column right">
+					<div class="example-entry">
+						<h2>Instant messaging and chat</h2>
+						<p>Socket.IO's "Hello world" is a chat app in just a few lines of code.</p>
+					</div>
 
-			<h2>STAY UP TO DATE</h2>
-			<p>Leave your email to hear about <b>new releases</b> and important <b>security</b> updates. <b>Very low traffic.</b></p>
+					<div class="example-entry">
+						<h2>Document collaboration</h2>
+						<p>Allow users to concurrently edit a document and see each other's changes.</p>
+					</div>
+				</div>
+			</div><!-- #examples -->
 
-			<h2>Instant messaging and chat</h2>
-			<p>Socket.IO's "Hello world" is a chat app in just a few lines of code.</p>
+			<div id="information">
+				<div class="information-column left">
+					<div class="information-entry">
+						<h2>USED BY EVERYONE</h2>
+						<p>From Microsoft Office, Yammer, Zendesk, Trello... to hackathon winners and little startups.</p>
+						<p>One of the most powerful JavaScript frameworks on GitHub, and most depended-upon NPM module.</p>
+					</div>
 
-			<h2>Document collaboration</h2>
-			<p>Allow users to concurrently edit a document and see each other's changes.</p>
+					<div class="information-entry">
+						<h2>STAY UP TO DATE</h2>
+						<p>Leave your email to hear about <b>new releases</b> and important <b>security</b> updates. <b>Very low traffic.</b></p>
+					</div>
+				</div>
 
-			<h2>IMMENSELY POWERFUL, YET EASY TO USE</h2>
-			<p>Our getting started guide will show you how to create lots of amazing applications in fewer than 200 lines of code.</p>
-			<p>We're not making that up. Get started <a href="#">now</a>.</p>
+				<div class="information-column right">
+					<div class="information-entry">
+						<h2>IMMENSELY POWERFUL, YET EASY TO USE</h2>
+						<p>Our getting started guide will show you how to create lots of amazing applications in fewer than 200 lines of code.</p>
+						<p>We're not making that up. Get started <a href="#">now</a>.</p>
+					</div>
 
-			<h2>JOIN THE COMMUNITY</h2>
-			<p>
-			- Post to the <a href="#">mailing list</a> with questions or to show your work
-			- Real-time help? Find us on IRC at #socket.io on irc.freenode.net
-			- Contribute code or report issues on <a href="https://github.com/LearnBoost/socket.io">GitHub</a>
-			</p>
+					<div class="information-entry">
+						<h2>JOIN THE COMMUNITY</h2>
+						<p>
+						- Post to the <a href="#">mailing list</a> with questions or to show your work
+						- Real-time help? Find us on IRC at #socket.io on irc.freenode.net
+						- Contribute code or report issues on <a href="https://github.com/LearnBoost/socket.io">GitHub</a>
+						</p>
+					</div>
+				</div>
+			</div><!-- #exaples -->
 		</div><!-- .entry-content -->
 	</main><!-- #main -->
 </div><!-- #primary -->
