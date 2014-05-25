64e991b6268cb1778c589b675452a8727a629814
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-25 19:44:07 +0300

    Revert "reset"
    This reverts commit d80ec6fd7c71e712d69c3139ca453451c5c0fef0.
    
    Conflicts:
    	style.css

---
+++ content-page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package socket.io-website
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_subtitle( '<h2 class="entry-subtitle">', '</h2>', true ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'socket-io-website' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	<footer class="entry-footer">
+		<?php edit_post_link( __( 'Edit', 'socket-io-website' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
---
+++ content.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package socket.io-website
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php socket_io_website_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
+	<?php else : ?>
+	<div class="entry-content">
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'socket-io-website' ) ); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'socket-io-website' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	<?php endif; ?>
+
+	<footer class="entry-footer">
+		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$categories_list = get_the_category_list( __( ', ', 'socket-io-website' ) );
+				if ( $categories_list && socket_io_website_categorized_blog() ) :
+			?>
+			<span class="cat-links">
+				<?php printf( __( 'Posted in %1$s', 'socket-io-website' ), $categories_list ); ?>
+			</span>
+			<?php endif; // End if categories ?>
+
+			<?php
+				/* translators: used between list items, there is a space after the comma */
+				$tags_list = get_the_tag_list( '', __( ', ', 'socket-io-website' ) );
+				if ( $tags_list ) :
+			?>
+			<span class="tags-links">
+				<?php printf( __( 'Tagged %1$s', 'socket-io-website' ), $tags_list ); ?>
+			</span>
+			<?php endif; // End if $tags_list ?>
+		<?php endif; // End if 'post' == get_post_type() ?>
+
+		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'socket-io-website' ), __( '1 Comment', 'socket-io-website' ), __( '% Comments', 'socket-io-website' ) ); ?></span>
+		<?php endif; ?>
+
+		<?php edit_post_link( __( 'Edit', 'socket-io-website' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
