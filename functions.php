81539a25e859a726eb5f460bc5b3ffaddf08eada
Guillermo Rauch <rauchg@gmail.com>
2014-05-25 06:20:05 -0700

    remove unneeded scripts

--- functions.php
+++ functions.php
@@ -89,14 +89,7 @@ add_action( 'widgets_init', 'socket_io_website_widgets_init' );
  */
 function socket_io_website_scripts() {
 	wp_enqueue_style( 'socket-io-website-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'socket-io-website-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'socket-io-website-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	wp_enqueue_script( 'socket-io-website-script', get_template_directory_uri() . 'main.js', array(), '1', true );
 }
 add_action( 'wp_enqueue_scripts', 'socket_io_website_scripts' );
 
