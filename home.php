6e5d145a2dc9281936be8a99fe1f7b97ccb4d04d
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-25 20:54:03 +0300

    Add title and subtitle to home

--- home.php
+++ home.php
@@ -9,39 +9,46 @@
 <?php get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<p class="centered">
-		<i>Socket.IO enables real-time bidirectional event-based communication.</i>
-		<i class="smaller">It works on every platform, browser or device, focusing equally on reliability and speed.</i>
-		</p>
-		<h2>Real-time analytics</h2>
-		<p>Push data to clients that gets represented as real-time counters, charts or logs.</p>
+		<header class="entry-header">
+			<h1 class="entry-title">SOCKET.IO 1.0 IS HERE</h1>
+			<h2 class="entry-subtitle">FEATURING THE FASTEST AND MOST RELIABLE REAL-TIME ENGINE</h2>
+		</header><!-- .entry-header -->
 
-		<h2>Binary streaming</h2>
-		<p>Starting in 1.0, it's possible to send any blob back and forth: image, audio, video.</p>
+		<div class="entry-content">
+			<p class="centered">
+			<i>Socket.IO enables real-time bidirectional event-based communication.</i>
+			<i class="smaller">It works on every platform, browser or device, focusing equally on reliability and speed.</i>
+			</p>
+			<h2>Real-time analytics</h2>
+			<p>Push data to clients that gets represented as real-time counters, charts or logs.</p>
 
-		<h2>USED BY EVERYONE</h2>
-		<p>From Microsoft Office, Yammer, Zendesk, Trello... to hackathon winners and little startups.</p>
-		<p>One of the most powerful JavaScript frameworks on GitHub, and most depended-upon NPM module.</p>
+			<h2>Binary streaming</h2>
+			<p>Starting in 1.0, it's possible to send any blob back and forth: image, audio, video.</p>
 
-		<h2>STAY UP TO DATE</h2>
-		<p>Leave your email to hear about <b>new releases</b> and important <b>security</b> updates. <b>Very low traffic.</b></p>
+			<h2>USED BY EVERYONE</h2>
+			<p>From Microsoft Office, Yammer, Zendesk, Trello... to hackathon winners and little startups.</p>
+			<p>One of the most powerful JavaScript frameworks on GitHub, and most depended-upon NPM module.</p>
 
-		<h2>Instant messaging and chat</h2>
-		<p>Socket.IO's "Hello world" is a chat app in just a few lines of code.</p>
+			<h2>STAY UP TO DATE</h2>
+			<p>Leave your email to hear about <b>new releases</b> and important <b>security</b> updates. <b>Very low traffic.</b></p>
 
-		<h2>Document collaboration</h2>
-		<p>Allow users to concurrently edit a document and see each other's changes.</p>
+			<h2>Instant messaging and chat</h2>
+			<p>Socket.IO's "Hello world" is a chat app in just a few lines of code.</p>
 
-		<h2>IMMENSELY POWERFUL, YET EASY TO USE</h2>
-		<p>Our getting started guide will show you how to create lots of amazing applications in fewer than 200 lines of code.</p>
-		<p>We're not making that up. Get started <a href="#">now</a>.</p>
+			<h2>Document collaboration</h2>
+			<p>Allow users to concurrently edit a document and see each other's changes.</p>
 
-		<h2>JOIN THE COMMUNITY</h2>
-		<p>
-		- Post to the <a href="#">mailing list</a> with questions or to show your work
-		- Real-time help? Find us on IRC at #socket.io on irc.freenode.net
-		- Contribute code or report issues on <a href="https://github.com/LearnBoost/socket.io">GitHub</a>
-		</p>
+			<h2>IMMENSELY POWERFUL, YET EASY TO USE</h2>
+			<p>Our getting started guide will show you how to create lots of amazing applications in fewer than 200 lines of code.</p>
+			<p>We're not making that up. Get started <a href="#">now</a>.</p>
+
+			<h2>JOIN THE COMMUNITY</h2>
+			<p>
+			- Post to the <a href="#">mailing list</a> with questions or to show your work
+			- Real-time help? Find us on IRC at #socket.io on irc.freenode.net
+			- Contribute code or report issues on <a href="https://github.com/LearnBoost/socket.io">GitHub</a>
+			</p>
+		</div><!-- .entry-content -->
 	</main><!-- #main -->
 </div><!-- #primary -->
 
