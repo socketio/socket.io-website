6f0f4345839d2f2c492029b0e2f2100ce5093c51
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-26 02:33:17 +0300

    More aggressive fade out

--- js/index.js
+++ js/index.js
@@ -10,12 +10,11 @@
 		var currentY = window.scrollY;
 		$( '.fading' ).each( function(i, el) {
 			var elemY = $( el ).position().top;
-			var offset = elemY - currentY;
+			var offset = ( elemY / 2 ) - currentY;
 			if ( currentY <= 0 ) {
 				$( el ).css( 'opacity', 1 );
 			} else if ( offset > 0 ) {
-				$( el ).css( 'opacity', ( ( offset * 0.5 ) / elemY ).toFixed(2) );
-				console.log( ( ( offset * 0.5 ) / elemY ).toFixed(2) );
+				$( el ).css( 'opacity', ( ( offset * 1.0 ) / elemY ).toFixed(2) );
 			}
 		} );
 	} );
