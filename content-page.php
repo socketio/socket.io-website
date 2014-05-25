2cc47e7beb36220e8c214a754c95d20cc294aae3
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-25 11:19:24 +0300

    Page title and subtitle styles.

--- content-page.php
+++ content-page.php
@@ -9,6 +9,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_subtitle( '<h2 class="entry-subtitle">', '</h2>', true ); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
