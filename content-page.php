dc6983e72b477f14666577a7cf622e83572bea51
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-26 11:33:21 +0300

    No header by default in pages

--- content-page.php
+++ content-page.php
@@ -7,11 +7,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php the_subtitle( '<h2 class="entry-subtitle">', '</h2>', true ); ?>
-	</header><!-- .entry-header -->
-
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
