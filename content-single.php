e718c5408298485b2477a37e6ad2ea0f06ac026f
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-26 13:24:01 +0300

    Tweak blog

--- content-single.php
+++ content-single.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title( '<h2 class="entry-title centered">', '</h2>' ); ?>
 
 		<div class="entry-meta">
 			<?php socket_io_website_posted_on(); ?>
