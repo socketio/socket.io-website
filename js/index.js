06024a016c342cfd166449fcf01be6b160f38933
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-26 10:58:52 +0300

    style

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
-(function( $ ) {
-	function setHeaderHeight() {
-		$( '#screen-fill' ).css( 'height', $( window ).height() - 50 );
-	}
+(function($) {
+  function setHeaderHeight() {
+    $('#screen-fill').css('height', $(window).height() - 50);
+  }
 
-	$( document ).ready( setHeaderHeight );
-	$( window ).resize( setHeaderHeight );
+  $(document).ready(setHeaderHeight);
+  $(window).resize(setHeaderHeight);
 
-	$( window ).scroll( function() {
-		var currentY = window.scrollY;
-		$( '.fading' ).each( function(i, el) {
-			var elemY = Math.round( $( el ).position().top );
-			var offset = ( elemY / 2 ) - currentY;
-			if ( currentY <= 0 ) {
-				$( el ).css( 'opacity', 1 );
-			} else if ( offset > 0 ) {
-				$( el ).css( 'opacity', ( ( offset * 1.0 ) / elemY ).toFixed(2) );
-			} else {
-				$( el ).css( 'opacity', 0 );
-			}
-		} );
-	} );
-} )( jQuery );
+  $(window).scroll(function() {
+    var currentY = window.scrollY;
+    $('.fading').each(function(i, el) {
+      var elemY = Math.round($(el).position().top);
+      var offset = (elemY / 2) - currentY;
+      if (currentY <= 0) {
+        $(el).css('opacity', 1);
+      } else if (offset > 0) {
+        $(el).css('opacity', ((offset * 1.0) / elemY).toFixed(2));
+      } else {
+        $(el).css('opacity', 0);
+      }
+    });
+  });
+})(jQuery);
