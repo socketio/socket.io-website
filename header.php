9330e9439bd2666365c8e2689f58ce95c1e9e713
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-27 21:01:17 +0300

    Change title

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title>Socket.IO</title>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
