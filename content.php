db2dc167b7a25cb364f9508ae3942b68ef312466
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-27 20:57:04 +0300

    Fix blog excerpt titles

--- content.php
+++ content.php
@@ -6,7 +6,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		<?php the_title( sprintf( '<h2 class="excerpt-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta" style="margin-top: 20px; margin-bottom: 20px;">
