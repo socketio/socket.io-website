be509b9cbaf606d6f92ab00d79b856dac11f0819
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-27 11:30:30 +0300

    Add a8c link

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<span class="footer-left">SOCKET.IO IS OPEN-SOURCE (MIT) AND RUN BY <a href="#">CONTRIBUTORS</a></span>
-			<span class="footer-right">SUPPORTED BY<div id="a8c-image"></div></span>
+			<span class="footer-right"><a href="http://automattic.com">SUPPORTED BY<div id="a8c-image"></div></a></span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
