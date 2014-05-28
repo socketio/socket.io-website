46e41476782201c638c64ba2820b04a2a6c3c9b0
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-28 15:21:18 +0300

    Add follow button (twitter)

--- footer.php
+++ footer.php
@@ -11,6 +11,9 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="twitter-info">
+			<a href="https://twitter.com/socketio" class="twitter-follow-button" data-show-count="true" data-lang="en">Follow @socketio</a>
+		</div>
 		<div class="site-info">
 			<span class="footer-left">SOCKET.IO IS OPEN-SOURCE (MIT) AND RUN BY <a href="https://github.com/LearnBoost/socket.io/graphs/contributors">CONTRIBUTORS</a></span>
 			<span class="footer-right"><a href="http://automattic.com">SUPPORTED BY<div id="a8c-image"></div></a></span>
@@ -22,6 +25,8 @@
 
 <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 <script src="http://cdn.socket.io/socket.io-1.0.0-pre5.js"></script>
+<!-- Twitter script -->
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 <?php if (is_home()): ?>
 	<script src="/wp-content/themes/socket.io-website/js/home.js"></script>
