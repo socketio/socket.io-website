b71bfa1dc748aa1c1e30fb099860cbf5d0635779
Tony Kovanen <tonykovanen@hotmail.com>
2014-05-28 22:21:58 +0300

    Change default wp_title separator to mdash

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Socket.IO <?php wp_title(); ?></title>
+<title>Socket.IO<?php wp_title( ' — ' ); ?></title>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700' rel='stylesheet' type='text/css'>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
