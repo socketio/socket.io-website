2d359c61c57a106666331cf75c3798445232aae1
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2019-12-13 08:28:21 +0100

    docs: update sponsors

---
+++ update-sponsors.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const request = require('request-promise');
+
+const filename = 'sponsors.json';
+const absoluteFilename = path.resolve(__dirname, filename);
+
+const graphqlEndpoint = 'https://api.opencollective.com/graphql/v2';
+
+const graphqlQuery = `query account {
+  account(slug: "socketio") {
+    members(role: BACKER, accountType: ORGANIZATION, limit: 500) {
+      nodes {
+        id
+        account {
+          name
+          slug
+          website
+          imageUrl
+        }
+        totalDonations {
+          value
+        }
+        createdAt
+      }
+    }
+  }
+}`;
+
+const nodeToSponsor = node => ({
+  url: node.account.website,
+  img: node.account.imageUrl,
+  alt: node.account.name
+});
+
+const getAllSponsors = async () => {
+  const requestOptions = {
+    method: 'POST',
+    uri: graphqlEndpoint,
+    body: { query: graphqlQuery },
+    json: true
+  };
+
+  const result = await request(requestOptions);
+  return result.data.account.members.nodes;
+};
+
+const main = async () => {
+  console.log(`fetching sponsors from the graphql API`);
+  const nodes = await getAllSponsors();
+  console.log(`fetched ${nodes.length} sponsors`);
+
+  const sponsors = nodes
+    .filter(n => n.totalDonations.value >= 100)
+    .sort((a, b) => b.totalDonations.value - a.totalDonations.value)
+    .map(nodeToSponsor);
+
+  fs.writeFileSync(absoluteFilename, JSON.stringify(sponsors, null, 2));
+  console.log(`content written to ${absoluteFilename}`);
+}
+
+main();
