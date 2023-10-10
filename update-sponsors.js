036747929ba07734ceb55b2195d4ad4a6c209d05
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2023-10-10 08:37:52 +0200

    docs(sponsors): update list

--- update-sponsors.js
+++ update-sponsors.js
@@ -54,8 +54,9 @@ const customImages = new Map([
   ["casinobonusca", { url: "https://casinobonusca.com/", img: "/images/sponsors/casinobonusca.png", alt: "CasinoBonusCa" }],
   ["king10", { url: "https://kingcasinobonus.co.uk", img: "/images/sponsors/king10.png", alt: "KingCasinoBonus" }],
   ["gem-m", { url: "https://www.noneedtostudy.com/take-my-online-class/", img: "/images/sponsors/noneedtostudy.png", alt: "Pay someone to take my online class - NoNeedToStudy.com" }],
-  ["yana1", { url: "https://nongamstopcasinos.net/", img: "/images/sponsors/nongamstopcasinos.png", alt: "Non Gamstop Casinos" }],
+  ["yana1", { url: "https://nongamstopcasinos.net/gb/", img: "/images/sponsors/nongamstopcasinos.png", alt: "Non Gamstop Casinos" }],
   ["de2", { url: "https://www.slotozilla.com/de/software/novoline-de", img: "/images/sponsors/slotozilla.png", alt: "Novoline Spielautomaten" }],
+  ["david128", { url: "https://bulkfollows.com", img: "https://images.opencollective.com/david128/c45a0fd/logo.png", alt: "Bulkfollows" }],
 ]);
 
 const customLinks = {
