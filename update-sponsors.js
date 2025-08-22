eb911cf800e8db296a325e9eba2a2c553b9b86e5
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2025-08-22 09:30:10 +0200

    docs(sponsors): clean up

--- update-sponsors.js
+++ update-sponsors.js
@@ -49,61 +49,22 @@ const graphqlQuery = `query account {
 }`;
 
 const customImages = new Map([
-  ["gem-m", { url: "https://www.noneedtostudy.com/take-my-online-class/", img: "/images/sponsors/noneedtostudy.png", alt: "Pay someone to take my online class - NoNeedToStudy.com" }],
-  ["yana1", { url: "https://nongamstopcasinos.net/gb/", img: "/images/sponsors/nongamstopcasinos.png", alt: "Non Gamstop Casinos" }],
-  ["de2", { url: "https://www.slotozilla.com/de/software/novoline-de", img: "/images/sponsors/slotozilla.png", alt: "Novoline Spielautomaten" }],
-  ["guest-3f7631a8", { url: "https://www.hityah.com", img: "/images/sponsors/guest-3f7631a8-2.jpg", alt: "Hityah" }],
   ["skweezer-net", { url: "https://skweezer.net/buy-instagram-followers", img: "/images/sponsors/skweezer-net.png", alt: "visit skweezer to buy instagram followers today" }],
-  ["buy-instagram-reels-views", { url: "https://expressfollowers.com/buy-instagram-reel-views/", img: "/images/sponsors/buy-instagram-reels-views.ico", alt: "Buy Instagram Reel Views" }],
-  ["guest-5c8f6e13", { url: "https://useviral.com/", img: "https://images.opencollective.com/useviral/29bff43/logo.png", alt: "UseViral" }],
-  ["user-53231496", { url: "https://justanotherpanel.com", img: "/images/sponsors/justanotherpanel.png", alt: "JustAnotherPanel" }],
-  ["karan2", { url: "https://givemeboost.com/buy-youtube-subscribers", img: "/images/sponsors/givemeboost.png", alt: "Give me boost" }],
   ["route4me", { url: "https://route4me.com", img: "/images/sponsors/route4me.png", alt: "Route Planner and Route Optimizer" }],
   ["user-62981c05", { url: "https://superviral.io/", img: "/images/sponsors/superviral.jpg", alt: "Superviral" }],
-  ["bloodycase", { url: "https://bloodycase.com/?promocode=SOCKETIO", img: "/images/sponsors/bloodycase.png", alt: "BloodyCase" }],
 ]);
 
 const customLinks = {
-  "veselin-lalev": {
-    url: "https://casinodaddy.com",
-    img: "/images/sponsors/casinodaddy.png",
-    alt: "Casino Daddy"
-  },
-  "quickbooks-tool-hub": {
-    url: "https://quickbookstoolhub.com/",
-    img: "/images/sponsors/quickbookstoolhub.png",
-    alt: "Quickbooks Tool Hub"
-  },
-  "leafletcasino-com": {
-    url: "https://leafletcasino.com/",
-    img: "https://images.opencollective.com/leafletcasino-com/f8cd951/logo.png",
-    alt: "leafletcasino.com"
-  },
   "casinotest-ltd": {
     url: "https://www.casinotest.com/",
     img: "https://images.opencollective.com/casinotest-ltd/7e3c899/logo.png",
     alt: "CasinoTest Ltd."
-  },
-  "automatenspielexcom": {
-    url: "https://automatenspielex.com",
-    img: "/images/sponsors/automatenspielexcom.png",
-    alt: "automatenspielex.com"
   },
   "veepn-vpn": {
     url: "https://veepn.com/vpn-apps/download-vpn-for-pc/",
     img: "https://images.opencollective.com/veepn-vpn/5e3715a/avatar.png",
     alt: "VeePN VPN"
   },
-  andynichols: {
-    url: "https://hmkasinotsuomi.com",
-    img: "/images/sponsors/holymolycasinos.png",
-    alt: "Suomalaiset kasinot"
-  },
-  auscasinos: {
-    url: "https://www.play-it-safe.net/",
-    img: "https://images.opencollective.com/auscasinos/8df0f47/logo.jpg",
-    alt: "AusCasinos"
-  }
 }
 
 const nodeToSponsor = node => (customLinks[node.account.slug] || {
