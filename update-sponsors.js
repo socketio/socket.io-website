8a07d5bf68123968ad0eb1f2006ab38a348cd323
Damien Arrachequesne <damien.arrachequesne@gmail.com>
2025-10-06 08:30:13 +0200

    docs(sponsors): use pagination when fetching the list of sponsors

--- update-sponsors.js
+++ update-sponsors.js
@@ -1,145 +1,193 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const request = require('request-promise');
+const fs = require("fs");
+const path = require("path");
+const request = require("request-promise");
 
-const filename = 'sponsors.json';
+const filename = "sponsors.json";
 const absoluteFilename = path.resolve(__dirname, filename);
 
-const graphqlEndpoint = 'https://api.opencollective.com/graphql/v2';
+const graphqlEndpoint = "https://api.opencollective.com/graphql/v2";
 
-const graphqlQuery = `query account {
-  account(slug: "socketio") {
-    orders(status: [ACTIVE, PAID], minAmount: 10000, limit: 1000) {
-      nodes {
-        fromAccount {
-          id
-        }
-        status
-        processedAt
-        totalAmount {
-          value
-        }
-      }
-    }
-    members(role: BACKER, limit: 1000) {
-      nodes {
-        tier {
-          name
-        }
-        account {
-          id
-          name
-          slug
-          website
-          socialLinks {
-            type
-            url
+function createGraphqlQuery(page, pageSize) {
+  const offset = (page - 1) * pageSize;
+  return `query account {
+    account(slug: "socketio") {
+      members(role: BACKER, offset: ${offset}, limit: ${pageSize}) {
+        nodes {
+          tier {
+            name
           }
-          imageUrl(format: jpg)
+          account {
+            id
+            name
+            slug
+            website
+            socialLinks {
+              type
+              url
+            }
+            imageUrl(format: jpg)
+          }
+          totalDonations {
+            value
+          }
+          since
+          isActive
         }
-        totalDonations {
-          value
-        }
-        createdAt
       }
     }
-  }
-}`;
+  }`;
+}
 
 const customImages = new Map([
-  ["skweezer-net", { url: "https://skweezer.net/buy-instagram-followers", img: "/images/sponsors/skweezer-net.png", alt: "visit skweezer to buy instagram followers today" }],
-  ["route4me", { url: "https://route4me.com", img: "/images/sponsors/route4me.png", alt: "Route Planner and Route Optimizer" }],
-  ["user-62981c05", { url: "https://superviral.io/", img: "/images/sponsors/superviral.jpg", alt: "Superviral" }],
+  [
+    "skweezer-net",
+    {
+      url: "https://skweezer.net/buy-instagram-followers",
+      img: "/images/sponsors/skweezer-net.png",
+      alt: "visit skweezer to buy instagram followers today",
+    },
+  ],
+  [
+    "route4me",
+    {
+      url: "https://route4me.com",
+      img: "/images/sponsors/route4me.png",
+      alt: "Route Planner and Route Optimizer",
+    },
+  ],
+  [
+    "user-62981c05",
+    {
+      url: "https://superviral.io/",
+      img: "/images/sponsors/superviral.jpg",
+      alt: "Superviral",
+    },
+  ],
 ]);
 
 const customLinks = {
   "casinotest-ltd": {
     url: "https://www.casinotest.com/",
     img: "https://images.opencollective.com/casinotest-ltd/7e3c899/logo.png",
-    alt: "CasinoTest Ltd."
+    alt: "CasinoTest Ltd.",
   },
   "veepn-vpn": {
     url: "https://veepn.com/vpn-apps/download-vpn-for-pc/",
     img: "https://images.opencollective.com/veepn-vpn/5e3715a/avatar.png",
-    alt: "VeePN VPN"
+    alt: "VeePN VPN",
   },
-}
+};
 
-const nodeToSponsor = node => (customLinks[node.account.slug] || {
-  url: node.account.socialLinks.find(link => link.type === "WEBSITE")?.url || node.account.website,
-  img: node.account.imageUrl,
-  alt: node.account.name
-});
+const nodeToSponsor = (node) =>
+  customLinks[node.account.slug] || {
+    url:
+      node.account.socialLinks.find((link) => link.type === "WEBSITE")?.url ||
+      node.account.website,
+    img: node.account.imageUrl,
+    alt: node.account.name,
+  };
 
 function monthDiff(dateFrom, dateTo) {
-  return dateTo.getMonth() - dateFrom.getMonth() + (12 * (dateTo.getFullYear() - dateFrom.getFullYear()));
+  return (
+    dateTo.getMonth() -
+    dateFrom.getMonth() +
+    12 * (dateTo.getFullYear() - dateFrom.getFullYear())
+  );
 }
 
 const NOW = new Date();
 const AMOUNT_PER_MONTH = 100;
 
+function sortByTotalDonationsOrStartDate(a, b) {
+  const sortByDonations = b.totalDonations.value - a.totalDonations.value;
+  const isSufficientDiff = Math.abs(sortByDonations) > AMOUNT_PER_MONTH;
+
+  if (isSufficientDiff) {
+    return sortByDonations;
+  }
+
+  return a.since.localeCompare(b.since);
+}
+
 const main = async () => {
   console.log(`fetching sponsors from the graphql API`);
 
-  const result = await request({
-    method: 'POST',
-    uri: graphqlEndpoint,
-    body: { query: graphqlQuery },
-    json: true
-  });
+  const validMembers = new Map();
 
-  const sponsors = result.data.account.members.nodes;
-  const orders = result.data.account.orders.nodes;
+  for (let i = 1; i < 20; i++) {
+    const result = await request({
+      method: "POST",
+      uri: graphqlEndpoint,
+      body: { query: createGraphqlQuery(i, 500) },
+      json: true,
+    });
 
-  const activeSponsorsById = new Set();
+    const sponsors = result.data.account.members.nodes;
 
-  orders.forEach(order => {
-    const isActiveSubscription = order.status === "ACTIVE";
-    const isSufficientOneShotPayment = order.totalAmount.value / (monthDiff(new Date(order.processedAt), NOW) || 1) >= AMOUNT_PER_MONTH;
+    console.log(`fetched ${sponsors.length} sponsors (page #${i})`);
 
-    if (isActiveSubscription || isSufficientOneShotPayment) {
-      activeSponsorsById.add(order.fromAccount.id);
+    if (sponsors.length === 0) {
+      break;
     }
-  });
 
-  const activeSponsors = sponsors
-    .map(n => {
-      const customImage = customImages.get(n.account.slug);
-      if (customImage) {
-        if (customImage.img) {
-          n.account.imageUrl = customImage.img;
+    sponsors
+      .map((node) => {
+        const customImage = customImages.get(node.account.slug);
+        if (customImage) {
+          if (customImage.img) {
+            node.account.imageUrl = customImage.img;
+          }
+          if (customImage.url) {
+            node.account.website = customImage.url;
+          }
+          if (customImage.alt) {
+            node.account.name = customImage.alt;
+          }
         }
-        if (customImage.url) {
-          n.account.website = customImage.url;
-        }
-        if (customImage.alt) {
-          n.account.name = customImage.alt;
-        }
-      }
-      return n;
-    })
-    .filter(n => {
-      const isSponsor = !n.tier || n.tier.name.toLowerCase() === 'sponsors';
-      const isActive = activeSponsorsById.delete(n.account.id); // prevent duplicates
-      const hasWebsite = n.account.socialLinks.some(link => link.type === "WEBSITE") || n.account.website; // website attribute is deprecated but still used in some cases
+        return node;
+      })
+      .filter((node) => {
+        const isSubscriptionActive =
+          node.tier && node.tier.name === "Sponsors" && node.isActive;
+        const totalDonations = node.totalDonations.value;
+        const durationInMonths = monthDiff(new Date(node.since), NOW);
+        const hasSufficientOneTimeDonation =
+          totalDonations / durationInMonths >= AMOUNT_PER_MONTH;
+        const hasWebsite =
+          node.account.socialLinks.some((link) => link.type === "WEBSITE") ||
+          node.account.website; // website attribute is deprecated but still used in some cases
 
-      return isSponsor && isActive && hasWebsite;
-    })
-    .sort((a, b) => {
-      const sortByDonation = b.totalDonations.value - a.totalDonations.value;
-      if (Math.abs(sortByDonation) > AMOUNT_PER_MONTH) {
-        return sortByDonation;
-      }
-      return a.createdAt.localeCompare(b.createdAt);
-    })
+        if (
+          (isSubscriptionActive || hasSufficientOneTimeDonation) &&
+          !hasWebsite
+        ) {
+          console.log(
+            `${node.account.name} (https://opencollective.com/${node.account.slug}) has no website`,
+          );
+        }
+
+        return (
+          (isSubscriptionActive || hasSufficientOneTimeDonation) && hasWebsite
+        );
+      })
+      .forEach((node) => {
+        if (!validMembers.has(node.account.id)) {
+          // prevent duplicates, as some sponsors appear twice with { "tier": null } and { "tier": { "name": "Sponsors" }}
+          validMembers.set(node.account.id, node);
+        }
+      });
+  }
+
+  const activeSponsors = [...validMembers.values()]
+    .sort(sortByTotalDonationsOrStartDate)
     .map(nodeToSponsor);
 
-  console.log(`${activeSponsors.length} active sponsors out of ${sponsors.length}`);
+  console.log(`found ${activeSponsors.length} active sponsors`);
 
   fs.writeFileSync(absoluteFilename, JSON.stringify(activeSponsors, null, 2));
   console.log(`content written to ${absoluteFilename}`);
-}
+};
 
 main();
